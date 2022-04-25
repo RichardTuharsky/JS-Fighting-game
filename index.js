387741b1fd909254eab77e5286c518ecea5d9a49
Richard <81489104+RichardTuharsky@users.noreply.github.com>
2022-04-25 20:58:31 +0200

    .

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ function animate() {
          player.isAttacking
         ){
          player.isAttacking = false
-         console.log('go')
+         document.querySelector('#enemyHealth').style.width = '20%'
     }
     if(
       rectanfularCollision ({
